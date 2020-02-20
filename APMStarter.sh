f2861755900bc409f7f0ab01fffe0e324bd7b59f
Student <student@stewart4.istlabs.rit.edu>
2020-02-20 16:13:05 -0500

    Added functions for APMStart

--- APMStarter.sh
+++ APMStarter.sh
@@ -4,3 +4,15 @@
 # Abbey Sands
 # Brennan Jackson
 # APMStarter - Starts the app monitoring tool
+
+# trap for exit - call cleanup
+trap 'cleanup' EXIT
+
+# Start the 6 C scripts
+start_c_scripts () {}
+
+# start the monitor scripts
+start_monitor_scripts () {}
+
+# Exit trap function - kills c scripts and monitor scripts
+cleanup () {}
