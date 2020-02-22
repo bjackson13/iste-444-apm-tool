e17b6c209983a238d212d6d7002fa823ad437f62
BrenBren <btj9560@rit.edu>
2020-02-21 21:12:23 -0500

    SystemMonitor now gets all system metrics and echos them out every 5 seconds

--- System/SystemMonitor.sh
+++ System/SystemMonitor.sh
@@ -4,3 +4,29 @@
 # Abbey Sands
 # Brennan Jackson
 # SystemMonitor - monitors system resource usage
+
+# IP address passed to script
+ip=$1
+
+# Get the network interface card based on the ip address passed in
+nic=$( ifconfig | grep -B 1 "$ip" | awk -F: 'NR==1 {print $1}' )
+
+while true
+do
+	#TODO:Ask about Timing - ifstat has built in delay, around 135s it adds on extra second - clarify against document as well
+
+	# get network utilizatio with sample after 1 second
+	bandwidth=$( ifstat -i $nic 5 1 | awk 'NR>2 {print $1" "$2}' )	
+	rx=$( echo $bandwidth | cut -d " " -f 1 )
+	tx=$( echo $bandwidth | cut -d " " -f 2 )
+	
+	# Get the disk writes in kB/s
+	diskwrites=$( iostat -d sda | grep sda | awk '{print $4}' )
+       	
+	
+	# Get disk utilization for '/' mount in MB
+	diskutil=$( df -BM / | awk 'NR==2{print $4}' | egrep -o [0-9] | tr -d "\n" )
+
+	echo "$SECONDS, $rx, $tx, $diskwrites, $diskutil"
+done
+
