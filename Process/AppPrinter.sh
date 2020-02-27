be846e40776c865a7ad2318cd23a1057921e9eea
Brennan Jackson <btj9560@rit.edu>
2020-02-27 16:58:26 -0500

    fixed somthing

--- Process/AppPrinter.sh
+++ Process/AppPrinter.sh
@@ -5,19 +5,13 @@
 # Brennan Jackson
 # AppPrinter - prints results sent by the app monitor to a csv file
 
-remove_temp () {
-        rm -f p_temp.txt
-}
-
-trap remove_temp EXIT
-
-
-while sleep 5
+while true
 do
 	for i in {1..6}
 	do
-	./Process/AppMonitor.sh $i > p_temp.txt 2> /dev/null
+		results=$(./Process/AppMonitor.sh $i)
 
-	echo "$SECONDS,$(<p_temp.txt)" >> ./APM"$i"_metrics.csv
+		echo "$SECONDS,$results" >> ./APM"$i"_metrics.csv
 	done
+	sleep 5
 done
