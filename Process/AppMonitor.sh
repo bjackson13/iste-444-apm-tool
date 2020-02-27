ab0ef277cca57ea53eda02471f935b34436c54ea
Student <student@localhost.localdomain>
2020-02-27 15:12:53 -0500

    WIP for printer and moniter service

--- Process/AppMonitor.sh
+++ Process/AppMonitor.sh
@@ -4,3 +4,12 @@
 # Abbey Sands
 # Brennan Jackson
 # AppMonitor - monitors the resources used by the 6 C scripts
+
+
+process=$(egrep "APM$1")
+
+# individual process cpu and memory usage
+memory=$(echo $apm_list | awk '{print $3}')
+cpu=$(echo $apm_list | awk '{print $4}')
+
+echo $memory,$cpu
--- Process/AppPrinter.sh
+++ Process/AppPrinter.sh
@@ -4,3 +4,18 @@
 # Abbey Sands
 # Brennan Jackson
 # AppPrinter - prints results sent by the app monitor to a csv file
+
+
+# gets list of all processes
+process_list=$(echo ps aux)
+apm_list=$($process_list | egrep APM[0-9])
+
+for i in {1..6}
+do
+./AppMonitor ${i}> temp.txt 2> /dev/null &
+
+echo "$SECONDS,$(<temp.txt)" >> ./APM${i}_metrics.csv
+
+#delay script 5 seconds
+sleep 5
+done
